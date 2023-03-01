106d2b6c1fd6be1c2acd4c442f46e57a142c6a20
Georgii <georgii@percents.com>
2023-03-01 15:00:00 -0500

    Test

--- Package.swift
+++ Package.swift
@@ -9,12 +9,23 @@ let package = Package(
     products: [
         .library(
             name: "PercentsRewards",
-            targets: ["PercentsRewards"])
+            targets: ["PercentsRewardsTargets"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/lorenzofiamingo/swiftui-cached-async-image", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         .binaryTarget(
             name: "PercentsRewards",
             path: "PercentsRewards.xcframework"
+        ),
+        .target(
+            name: "PercentsRewardsTargets",
+            dependencies: [
+                .target(name: "PercentsRewards"),
+                .product(name: "CachedAsyncImage", package: "swiftui-cached-async-image")
+            ],
+            path: "Sources"
         )
     ]
 )
---
+++ Sources/Placeholder.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Georgii Fesenko on 3/1/23.
+//
+
+import Foundation
